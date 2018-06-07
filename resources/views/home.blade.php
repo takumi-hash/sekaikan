14ed7abf1114bce3c4be210ab02e438ab1735edf
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-07 07:06:18 +0000

    menu column added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,63 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css" type="text/css" />
+    <title>Welcome to TeamSEKAIKAN!</title>
 
-                    You are logged in!
-                </div>
-            </div>
+</head>
+
+<body>
+    <div id="logo">
+        <h1 class="text-white text-center py-2"><span class="font-weight-light">TEAM</span><span class="font-weight-bold">SEKAIKAN</span></h1>
+    </div>
+    <div class="column-container">
+        <div class="column p-3" id="manatee">
+            <h2 class="font-weight-light display-4">Manatee</h2>
+            <p>I am Manatee!</p>
+            <p class="profile-btn">
+                <a class="btn btn-success display-block" href="#">Web Page</a>
+                <a class="btn btn-warning" href="#">Github</a>
+                <a class="btn btn-info" href="#">Facebook</a></p>
+        </div>
+        <div class="column p-3" id="maho">
+            <h2 class="font-weight-light display-4">Maho</h2>
+            <p>I love coffee!</p>
+            <p class="profile-btn"><a class="btn btn-info" href="#">Web Page</a></p>
+        </div>
+        <div class="column p-3" id="saaya">
+            <h2 class="font-weight-light display-4">Saaya</h2>
+            <p>I can speak Korean.</p>
+            <p class="profile-btn"><a class="btn btn-info" href="#">Web Page</a></p>
+        </div>
+        <div class="column p-3" id="jun">
+            <h2 class="font-weight-light display-4">Jun</h2>
+            <p>My Japanese is pretty good.</p>
+            <p class="profile-btn"><a class="btn btn-info" href="#">Web Page</a></p>
+        </div>
+        <div class="column p-3" id="takumi">
+            <h2 class="font-weight-light display-4">Takumi</h2>
+            <p>I love movies.</p>
+            <p class="profile-btn"><a class="btn btn-info" href="#">Web Page</a></p>
+        </div>
+        <div class="column p-3" id="erika">
+            <h2 class="font-weight-light display-4">Erika</h2>
+            <p>Call me beth.</p>
+            <p class="profile-btn"><a class="btn btn-info" href="#">Web Page</a></p>
         </div>
     </div>
-</div>
-@endsection
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,67 @@
+<?php
+$image = array ('image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg');
+$rand = mt_rand(0,3);
+?>
+ 
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Sekaikan</title>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+  <title>Hello, world!</title>
+  <style type="text/css">
+    body{
+      background: linear-gradient(-45deg, rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url(images/{{$image[$rand]}});
+      background-position: top center;
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: cover;
+      color: white;
+      margin-bottom: 0;
+      height:100vh;
+    }
+  </style>
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-extend ml-5 position-relative">
+    <a class="navbar-brand" href="#"><span class="display-4">SEKAIKAN</span></a>
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
+            <ul class="navbar-nav position-absolute">
+                @auth 
+                    <li class="nav-item"><a class="nav-link" href="{{ url('/home') }}">Home</a></li>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                    <li class="nav-item active"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                @endauth
+            </ul>
             @endif
+  </nav>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Sekaikan
-                </div>
+  <div class="container-fluid text-center">
+    <div class="jumbotron jumbotron-extend">
+      <div id="logo">
+        <h1 class="display-1 font-weight-bold text-left">HELLO<br>WE ARE SEKAIKAN.</h1>
+        <p class="lead text-left mt-3">'18 NEW GRADS CODING TRAINING</p>
+        
+        <!--p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p-->
+      </div>
+    </div>
+  </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+
 </html>
